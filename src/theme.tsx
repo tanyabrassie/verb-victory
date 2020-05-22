1ff5c2c1e9bdf3fad7df3034d11bbcfb01a1aee1
Tanya Brassie <tbrassie@empatico.org>
2020-05-22 11:17:54 -0400

    Adding theme and fonts

---
+++ src/theme.tsx
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    lightRed: '#fff6f3',
+    black: '#000000',
+  },
+  space: [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50],
+  fonts: {
+    sourceSans: '`Source Sans Pro`, sans-serif',
+  }
+};
